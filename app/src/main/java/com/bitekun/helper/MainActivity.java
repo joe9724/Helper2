8c2f07856852832a82c19a91274f22eb03d2533f
joe9724 <64060764@qq.com>
2017-09-29 23:44:34 +0800

    ddd

--- app/src/main/java/com/bitekun/helper/MainActivity.java
+++ app/src/main/java/com/bitekun/helper/MainActivity.java
@@ -14,12 +14,18 @@ import com.bitekun.helper.R;
 import com.bitekun.helper.adapter.AdapterGV;
 import com.bitekun.helper.bean.GridItem;
 
+import scut.carson_ho.searchview.ICallBack;
+import scut.carson_ho.searchview.SearchView;
+import scut.carson_ho.searchview.bCallBack;
+
 
 public class MainActivity extends Activity implements View.OnClickListener
 {
 
  RelativeLayout layout_xcqd,layout_kfcj,layout_kfdx,layout_wfwd,layout_yjfk,layout_zx;
  Intent i;
+	private SearchView searchView;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
@@ -38,6 +44,27 @@ public class MainActivity extends Activity implements View.OnClickListener
 		layout_wfwd.setOnClickListener(this);
 		layout_yjfk.setOnClickListener(this);
 		layout_zx.setOnClickListener(this);
+
+		//
+		// 3. 绑定组件
+		searchView = (SearchView) findViewById(R.id.search_view);
+
+		// 4. 设置点击键盘上的搜索按键后的操作（通过回调接口）
+		// 参数 = 搜索框输入的内容
+		searchView.setOnClickSearch(new ICallBack() {
+			@Override
+			public void SearchAciton(String string) {
+				System.out.println("我收到了" + string);
+			}
+		});
+
+		// 5. 设置点击返回按键后的操作（通过回调接口）
+		searchView.setOnClickBack(new bCallBack() {
+			@Override
+			public void BackAciton() {
+				finish();
+			}
+		});
 
 
 	}
